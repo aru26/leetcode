4c81db3191aa38b257a3996f87e7b814573702e3
aru26 <50775209+aru26@users.noreply.github.com>
2020-05-29 08:48:53 +0530

    Update Subtree of Another Tree.py

--- Subtree of Another Tree.py
+++ Subtree of Another Tree.py
@@ -37,3 +37,33 @@ class Solution(object):
     
     # def traverse(self, s, t):
     #     return s is not None and (self.equals(s, t) or self.traverse(s.left, t) or self.traverse(s.right, t))
+    
+    
+    """
+    # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def isSubtree(self, s , t)->bool:
+        if s is None and t is None:
+            return True
+        if t is None:
+            return True
+        if  s is None and t is not None:
+            return False
+        return self.issame(s,t) or self.isSubtree(s.left,t)or
+    self.isSubtree(s.right,t)
+       
+        def issame(self,s,t):
+            if s is None and t is None:
+                return True
+            if s is None or t is None:
+                return False
+            return s.val == t.val and self.issame(s.left,t.left)==
+        self.issame(s.right,t.right)
+    
+    """
