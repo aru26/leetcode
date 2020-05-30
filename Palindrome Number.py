a16d9811ab66641853889a27d9347f5213054290
aru26 <50775209+aru26@users.noreply.github.com>
2020-05-30 11:44:33 +0530

    Create Palindrome Number.py

---
+++ Palindrome Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        if(x < 0):
+            return False
+        else:
+            x = str(x)
+            return x[::-1] == x
