2ac06ff5cbefb9cd91967ff71fab2c1e3a2761a8
Arushi Dwivedi <50775209+aru26@users.noreply.github.com>
2020-06-21 08:13:13 +0530

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -1,3 +1,38 @@
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        
+        """
+            1. build a hashmap to record the frequenc of each letter
+            2. Use heap, use min heap and keep the size as k
+            
+            Time: O(nlogk)
+            Space: O(n)
+        """
+        
+        # 1. build a hashmap to record the frequenc of each letter
+        dic = {}
+        for num in nums:
+            if num in dic:
+                dic[num] += 1
+            else:
+                dic[num] = 1
+        print(dic)
+        
+        # top k most frequency words, [1,2], put the frequency into the heap
+        heap = []
+        for key, frequency in dic.items():
+            heappush(heap, (frequency, key))
+            if len(heap) > k:
+                heappop(heap)
+            
+        result = []
+        for _, element in heap:
+            result.append(element)
+        return result
+
+========================================================================================================================================
 Given a non-empty array of integers, return the k most frequent elements.
 
 Example 1:
