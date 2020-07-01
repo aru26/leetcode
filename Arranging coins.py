72274150e91be9d65aaad41aa1f002cb1d31d439
Arushi Dwivedi <50775209+aru26@users.noreply.github.com>
2020-07-01 21:03:40 +0530

    Create Arranging coins.py

---
+++ Arranging coins.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def check(mid, n):
+            return mid*(mid+1) <= 2*n
+
+        left, right = 1, n
+        while left <= right:
+            mid = left + (right-left)//2
+            if not check(mid, n):
+                right = mid-1
+            else:
+                left = mid+1
+        return right
