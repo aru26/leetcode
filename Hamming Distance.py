d7d0358bedfcb2aaaed719032d91bdc03025848f
Arushi Dwivedi <50775209+aru26@users.noreply.github.com>
2020-07-05 17:02:23 +0530

    Create   Hamming Distance.py

---
+++ Hamming Distance.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        distance = 0
+        z = x ^ y
+        while z:
+            distance += 1
+            z &= z - 1
+        return distance
+        
