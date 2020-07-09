680ab0bd6f9a4309a231d3a3723fd3a1f5428e2c
Arushi Dwivedi <50775209+aru26@users.noreply.github.com>
2020-07-09 20:05:06 +0530

    Create Maximum width of binary tree.py

---
+++ Maximum width of binary tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def widthOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(node, i, depth, leftmosts):
+            if not node:
+                return 0
+            if depth >= len(leftmosts):
+                leftmosts.append(i)
+            return max(i-leftmosts[depth]+1, \
+                       dfs(node.left, i*2, depth+1, leftmosts), \
+                       dfs(node.right, i*2+1, depth+1, leftmosts))
+
+        leftmosts = []
+        return dfs(root, 1, 0, leftmosts)
